72b0fc1e51ffec0e7d91ec75c55828ef4f0da09c
Jimmy Bergström <thekwarf@gmail.com>
2020-12-02 22:28:09 +0100

    Day 1, Part 2 - Haskell (I discovered list comprehension!)

--- 2020/day1/main.hs
+++ 2020/day1/main.hs
@@ -1,15 +1,18 @@
-targetValue = (2020 -)
+findFirstAnswer list = head [ x * y | x <- list, y <- list, x + y == 2020]
 
-findFirstAnswer (x:xs) = if targetValue x `elem` xs
-    then x * targetValue x
-    else findFirstAnswer xs
+findSecondAnswer list = head [ x * y * z | x <- list, y <- list, z <- list, x + y + z == 2020]
 
 main = do
-    input <- readFile "input.txt"
+    input <- map (read :: String -> Int)
+        . lines
+        <$> readFile "input.txt"
 
     putStr "The answer to the first part is: "
     print
         . findFirstAnswer
-        . map (read :: String -> Int)
-        . lines
+        $ input
+
+    putStr "The answer to the second part is: "
+    print
+        . findSecondAnswer
         $ input
