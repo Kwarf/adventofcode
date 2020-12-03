8e4f7b3819079690a4655590e02bffee53f99a4d
Jimmy Bergström <thekwarf@gmail.com>
2020-12-03 12:17:33 +0100

    Day 3, Part 1 - Rust feels like home

---
+++ 2020/day3/src/main.rs
@@ -0,0 +1,115 @@
+use std::{fs::File, io::BufRead, io::BufReader, iter::FromIterator};
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum Token {
+    Empty,
+    Tree,
+}
+
+impl From<char> for Token {
+    fn from(value: char) -> Self {
+        match value {
+            '.' => Token::Empty,
+            '#' => Token::Tree,
+            _ => panic!("unexpected token"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Map {
+    tokens: Vec<Vec<Token>>,
+}
+
+impl Map {
+    fn token_at(&self, pos: &Position) -> Option<Token> {
+        if pos.y >= self.tokens.len() {
+            None
+        } else {
+            let width = self.tokens[0].len();
+            Some(self.tokens[pos.y][pos.x % width])
+        }
+    }
+}
+
+impl FromIterator<Vec<Token>> for Map {
+    fn from_iter<T: IntoIterator<Item = Vec<Token>>>(iter: T) -> Self {
+        Map {
+            tokens: iter.into_iter().collect(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Position {
+    x: usize,
+    y: usize,
+}
+
+struct StepSequence {
+    x: usize,
+    y: usize,
+    x_step: usize,
+    y_step: usize,
+}
+
+impl StepSequence {
+    fn new(x: usize, y: usize) -> StepSequence {
+        StepSequence {
+            x: 0,
+            y: 0,
+            x_step: x,
+            y_step: y,
+        }
+    }
+}
+
+impl Iterator for StepSequence {
+    type Item = Position;
+    fn next(&mut self) -> Option<Position> {
+        let p = Position {
+            x: self.x,
+            y: self.y,
+        };
+
+        self.x += self.x_step;
+        self.y += self.y_step;
+
+        Some(p)
+    }
+}
+
+fn parse_line<T: AsRef<str>>(line: T) -> Vec<Token> {
+    line.as_ref().chars().map(Token::from).collect()
+}
+
+fn main() {
+    let file = File::open("input.txt").expect("Input file not found");
+    let map = BufReader::new(file)
+        .lines()
+        .filter_map(|x| x.ok())
+        .map(parse_line)
+        .collect::<Map>();
+
+    let num_trees = StepSequence::new(3, 1)
+        .map(|x| map.token_at(&x))
+        .take_while(|x| x.is_some())
+        .filter(|x| x.unwrap() == Token::Tree)
+        .count();
+
+    println!("The answer to the first part is: {}", num_trees);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_step_sequence() {
+        let steps = StepSequence::new(3, 1).take(3).collect::<Vec<Position>>();
+
+        assert_eq!(Position { x: 0, y: 0 }, steps[0]);
+        assert_eq!(Position { x: 3, y: 1 }, steps[1]);
+        assert_eq!(Position { x: 6, y: 2 }, steps[2]);
+    }
+}
