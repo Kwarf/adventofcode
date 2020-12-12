52fce01bbe90fe0fc535c918ac84985a47754ab6
Jimmy Bergström <thekwarf@gmail.com>
2020-12-12 04:50:46 +0100

    Day 9, Part 2 - Haskell

--- 2020/day9/main.hs
+++ 2020/day9/main.hs
@@ -1,11 +1,24 @@
+import           Data.List                      ( sort )
+
 partOne list i = if target `notElem` sums then target else partOne list (i + 1)
  where
   target   = list !! (25 + i)
   preamble = take 25 $ drop i list
   sums     = [ x + y | x <- preamble, y <- preamble, x /= y ]
 
+partTwo list target i | target == sum sublist = head sorted + last sorted
+                      | sum sublist > target  = partTwo (drop 1 list) target 2
+                      | otherwise             = partTwo list target (i + 1)
+ where
+  sublist = take i list
+  sorted  = sort sublist
+
 main = do
   input <- map (read :: String -> Int) . lines <$> readFile "input.txt"
 
+  let invalidNo = partOne input 0
   putStr "The answer to the first part is: "
-  print $ partOne input 0
+  print invalidNo
+
+  putStr "The answer to the second part is: "
+  print $ partTwo input invalidNo 2
