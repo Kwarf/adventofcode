bee0de5dae69039c26f8b43ec7f6c58819f81647
Jimmy Bergström <thekwarf@gmail.com>
2020-12-12 03:24:07 +0100

    Day 7 - Cleanup

--- 2020/day7/src/main.rs
+++ 2020/day7/src/main.rs
@@ -1,63 +1,61 @@
 use std::{
     collections::{HashMap, HashSet},
+    convert::{TryFrom, TryInto},
     fs, iter,
-    str::FromStr,
 };
 
 use regex::Regex;
 
 #[derive(Debug, Clone)]
-struct BagRule {
-    color: String,
-    content: HashMap<String, usize>,
+struct BagRule<'a> {
+    color: &'a str,
+    content: HashMap<&'a str, usize>,
 }
 
-impl FromStr for BagRule {
-    type Err = ();
+impl<'a> TryFrom<&'a str> for BagRule<'a> {
+    type Error = &'static str;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+    fn try_from(s: &'a str) -> Result<Self, Self::Error> {
         let mut split = s.split(" bags contain ");
         Ok(BagRule {
-            color: split.next().ok_or_else(|| ())?.to_owned(),
+            color: split.next().ok_or_else(|| "could not parse color")?,
             content: Regex::new(r"(\d+) ([^,.]*) bags*")
-                .map_err(|_| ())?
-                .captures_iter(split.next().ok_or_else(|| ())?)
-                .filter_map(|x| {
-                    Some((
-                        x.get(2)?.as_str().to_owned(),
-                        x.get(1)?.as_str().parse::<usize>().ok()?,
-                    ))
-                })
-                .collect::<HashMap<String, usize>>(),
+                .unwrap()
+                .captures_iter(
+                    split
+                        .next()
+                        .ok_or_else(|| "could not parse contained bags")?,
+                )
+                .filter_map(|x| Some((x.get(2)?.as_str(), x.get(1)?.as_str().parse().ok()?)))
+                .collect::<HashMap<&'a str, usize>>(),
         })
     }
 }
 
-fn part_one(color: &str, rules: &[BagRule]) -> Vec<String> {
-    rules
-        .iter()
-        .filter(|x| x.content.contains_key(color))
-        .map(|x| x.color.clone())
-        .flat_map(|x| iter::once(x.clone()).chain(part_one(&x, rules)))
-        .collect::<Vec<String>>()
+fn part_one<'a>(color: &'a str, rules: &'a [BagRule]) -> Box<dyn Iterator<Item = &'a str> + 'a> {
+    Box::new(
+        rules
+            .iter()
+            .filter(move |&x| x.content.contains_key(color))
+            .flat_map(move |x| iter::once(x.color).chain(part_one(x.color, rules))),
+    )
 }
 
-fn part_two(color: &str, num: usize, rules: &HashMap<String, BagRule>) -> usize {
+fn part_two(color: &str, num: usize, rules: &HashMap<&str, BagRule>) -> usize {
     rules
         .get(color)
-        .unwrap()
+        .expect("attempted to look up non-existent color")
         .content
         .iter()
-        .fold(num, |acc, (x, n)| acc + part_two(&x, num * n, rules))
+        .fold(num, |acc, (&x, n)| acc + part_two(x, num * n, rules))
 }
 
-fn main() {
-    let rules = fs::read_to_string("input.txt")
-        .expect("Input file not found")
+fn main() -> Result<(), &'static str> {
+    let input = fs::read_to_string("input.txt").expect("Input file not found");
+    let rules = input
         .lines()
-        .filter_map(|x| x.parse::<BagRule>().ok())
-        .map(|x| (x.color.clone(), x))
-        .collect::<HashMap<String, BagRule>>();
+        .map(|s| s.try_into().and_then(|x: BagRule| Ok((x.color, x))))
+        .collect::<Result<HashMap<&str, BagRule>, &'static str>>()?;
 
     println!(
         "The answer to the first part is: {}",
@@ -65,8 +63,7 @@ fn main() {
             "shiny gold",
             &rules.values().cloned().collect::<Vec<BagRule>>()
         )
-        .iter()
-        .collect::<HashSet<&String>>()
+        .collect::<HashSet<&str>>()
         .len()
     );
 
@@ -74,4 +71,6 @@ fn main() {
         "The answer to the second part is: {}",
         part_two("shiny gold", 1, &rules) - 1
     );
+
+    Ok(())
 }
