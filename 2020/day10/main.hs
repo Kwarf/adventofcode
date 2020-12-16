e874903be396c7f073d0c03cfe2c7df885a46e24
Jimmy Bergström <thekwarf@gmail.com>
2020-12-16 01:12:30 +0100

    Day 10, Part 2 - Haskell

--- 2020/day10/main.hs
+++ 2020/day10/main.hs
@@ -1,3 +1,12 @@
+import           Data.IntSet                    ( findMax
+                                                , IntSet
+                                                , fromList
+                                                )
+import           Data.IntMap                    ( (!)
+                                                , findWithDefault
+                                                , fromSet
+                                                , IntMap
+                                                )
 import           Data.List                      ( sort )
 
 oneif b = if b then 1 else 0
@@ -12,8 +21,21 @@ partOne (o, t) (x : xs) = foldl partOne (no, nt) [xs]
   no   = if x + 1 == next then o + 1 else o
   nt   = if x + 3 == next then t + 1 else t
 
+partTwo list = map
+ where
+  t = findMax list
+  map = fromSet
+    (\x -> if x == t
+      then 1
+      else sum [ findWithDefault 0 (x + i) map | i <- [1 .. 3] ]
+    )
+    list
+
 main = do
   input <- map (read :: String -> Int) . lines <$> readFile "input.txt"
 
   putStr "The answer to the first part is: "
   print . tmul $ partOne (partOneInit input) (sort input)
+
+  putStr "The answer to the second part is: "
+  print $ partTwo (fromList $ 0 : maximum input + 3 : input) ! 0
