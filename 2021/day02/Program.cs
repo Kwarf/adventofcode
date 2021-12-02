49e38da80733d11c6c9ae0ca06e8f12f7a6866e1
Jimmy Bergström <me@kwarf.com>
2021-12-02 19:29:38 +0100

    Day 2, Part 1 & 2 - C#

---
+++ 2021/day02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var commands = File.ReadAllLines("input.txt")
+	.Select(Command.FromString)
+	.ToArray();
+
+Console.WriteLine($"The answer to the first part is: {commands.Aggregate(default(State), Command.ApplyForPartOne, x => x.Position * x.Depth)}");
+Console.WriteLine($"The answer to the second part is: {commands.Aggregate(default(State), Command.ApplyForPartTwo, x => x.Position * x.Depth)}");
+
+enum Direction { Horizontal, Vertical }
+
+record struct State(int Position = default, int Depth = default, int Aim = default);
+
+record struct Command(Direction Direction, int Delta)
+{
+	public static Command FromString(string line)
+	{
+		var parts = line.Split(' ');
+		var delta = int.Parse(parts[1]);
+
+		return new Command(parts[0] == "forward" ? Direction.Horizontal : Direction.Vertical
+			, parts[0] == "up" ? -delta : delta
+			);
+	}
+
+	public static State ApplyForPartOne(State state, Command command) => command.Direction switch
+	{
+		Direction.Horizontal => state with { Position = state.Position + command.Delta },
+		Direction.Vertical => state with { Depth = state.Depth + command.Delta },
+		_ => throw new InvalidOperationException(),
+	};
+
+	public static State ApplyForPartTwo(State state, Command command) => command.Direction switch
+	{
+		Direction.Horizontal => state with
+		{
+			Position = state.Position + command.Delta,
+			Depth = state.Depth + state.Aim * command.Delta,
+		},
+		Direction.Vertical => state with { Aim = state.Aim + command.Delta },
+		_ => throw new InvalidOperationException(),
+	};
+}
