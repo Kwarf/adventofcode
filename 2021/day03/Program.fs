a403d545d1b7d18c3c2fe2d5c37de615ccde6615
Jimmy Bergström <me@kwarf.com>
2021-12-03 18:37:04 +0100

    Day 3, Part 2 - F#

--- 2021/day03/Program.fs
+++ 2021/day03/Program.fs
@@ -4,26 +4,32 @@ open System.IO
 [<Struct>]
 type BitStatistics = { Positive : int; Negative: int }
 
-let intFromBinary x = Convert.ToInt32 (x, 2)
-
-let dataFrom path =
-    File.ReadAllLines path
-    |> Array.map intFromBinary
-
 let getBitStatistics value offset stats =
-    match value &&& (1 <<< offset) with
+    match value &&& (1 <<< 11 - offset) with
     | 0 -> { stats with Negative = stats.Negative + 1; }
     | _ -> { stats with Positive = stats.Positive + 1; }
 
-let calculateStatistics state value = Array.mapi (getBitStatistics value) state
+let summarizeStatistics = Array.fold (fun acc x -> Array.mapi (getBitStatistics x) acc) (Array.zeroCreate 12)
 
-let summarize op = Seq.rev >> Seq.fold (fun acc x -> (acc <<< 1) ||| (if op x.Positive x.Negative then 1 else 0)) 0
+let commonize op = Seq.fold (fun acc x -> (acc <<< 1) ||| (if op x.Positive x.Negative then 1 else 0)) 0
+
+let rec filterize op (input: int array) idx =
+    match input.Length with
+    | 1 -> input.[0]
+    | _ ->
+        let stats = Array.item idx (summarizeStatistics input)
+        let cond = match stats.Positive - stats.Negative with
+                   | 0 -> if op 1 0 then (<>) else (=)
+                   | _ -> if op stats.Positive stats.Negative then (<>) else (=)
+        filterize op (Array.filter (fun x -> cond (x &&& (1 <<< 11 - idx)) 0) input) (idx + 1)
 
 [<EntryPoint>]
 let main _ =
     let input =
-        dataFrom "input.txt"
-        |> Array.fold calculateStatistics (Array.zeroCreate 12)
+        File.ReadAllLines "input.txt"
+        |> Array.map (fun x -> Convert.ToInt32 (x, 2))
 
-    Console.WriteLine $"The answer to the first part is: {summarize (>) input * summarize (<) input}"
+    let day1 = summarizeStatistics input
+    Console.WriteLine $"The answer to the first part is: {commonize (>) day1 * commonize (<) day1}"
+    Console.WriteLine $"The answer to the second part is: {filterize (>) input 0 * filterize (<) input 0}"
     0
