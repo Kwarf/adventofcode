99452124b4daedd5724dbe169bb6c083d912bdd9
Jimmy Bergström <me@kwarf.com>
2021-12-05 00:27:03 +0100

    Day 4, Part 2 - Julia

--- 2021/day04/main.jl
+++ 2021/day04/main.jl
@@ -1,4 +1,5 @@
 mutable struct Board
+	wonAt::Int32
 	rows::Vector{Vector{Int32}}
 	columns::Vector{Vector{Int32}}
 end
@@ -7,24 +8,33 @@ input = readlines("input.txt")
 
 boards = Vector{Board}()
 for boardDefinition in Iterators.partition(input[3:end], 6)
-	board = Board(Vector{Vector{Int32}}(), Vector{Vector{Int32}}())
+	board = Board(0, Vector{Vector{Int32}}(), Vector{Vector{Int32}}())
 	for row in boardDefinition[1:end-1]
-		push!(board.rows, map((x) -> parse(Int32, x), split(strip(row), r"\s+")))
+		push!(board.rows, map(x -> parse(Int32, x), split(strip(row), r"\s+")))
 	end
 	board.columns = [x[:] for x in eachrow(hcat(board.rows...))]
 	push!(boards, board)
 end
 
-for number in map((x) -> parse(Int32, x), split(input[1], ","))
+winners = Vector{Board}()
+for number in map(x -> parse(Int32, x), split(input[1], ","))
 	for board in boards
 		f(x) = x != number
 		board.rows = [filter(f, x) for x in board.rows]
 		board.columns = [filter(f, x) for x in board.columns]
 
-		empty(x) = size(x, 1) == 0
-		if findfirst(empty, board.rows) != nothing || findfirst(empty, board.columns) != nothing
-			println("The answer to the first part is: ", sum([sum(x) for x in board.rows]) * number)
-			return
+		e(x) = size(x, 1) == 0
+		if findfirst(e, board.rows) != nothing || findfirst(e, board.columns) != nothing
+			board.wonAt = number
+			push!(winners, board)
 		end
 	end
+	deleteat!(boards, findall(x -> x in winners, boards))
 end
+
+function score(board)
+	return sum([sum(x) for x in board.rows]) * board.wonAt
+end
+
+println("The answer to the first part is: ", score(winners[1]))
+println("The answer to the second part is: ", score(winners[end]))
