22bc98006195ee03cefc7e5def1e12f8264c0343
Jimmy Bergström <me@kwarf.com>
2021-12-06 18:01:01 +0100

    Day 6, Part 2 - Haskell

--- 2021/day06/main.hs
+++ 2021/day06/main.hs
@@ -1,13 +1,18 @@
+{-# LANGUAGE TupleSections #-}
 import Data.List.Split (endBy)
+import Data.Map (assocs, fromListWith, elems)
 
-tick 0 = [6, 8]
-tick x = [x - 1]
+tick (0, a) = [(6, a), (8, a)]
+tick (k, a) = [(k - 1, a)]
 
 simulate 0 xs = xs
-simulate n xs = simulate (n - 1) xs >>= tick
+simulate n xs = simulate (n - 1) $ fromListWith (+) $ concatMap tick $ assocs xs
 
 main = do
-  seed <- map (read :: [Char] -> Int) . endBy "," <$> readFile "input.txt"
+  seed <- fromListWith (+) . map ((, 1) . (read :: [Char] -> Int)) . endBy "," <$> readFile "input.txt"
 
   putStr "The answer to the first part is: "
-  print $ length $ simulate 80 seed
+  print $ sum . elems $ simulate 80 seed
+
+  putStr "The answer to the second part is: "
+  print $ sum . elems $ simulate 256 seed
