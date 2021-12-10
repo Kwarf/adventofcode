7907204f5395c8926e90c84ec75798459df037c7
Jimmy Bergström <me@kwarf.com>
2021-12-10 18:20:06 +0100

    Day 9, Part 2 - Haskell

--- 2021/day09/main.hs
+++ 2021/day09/main.hs
@@ -1,5 +1,6 @@
 import Data.Char (isSpace)
 import Data.Foldable (find)
+import Data.List (nub, sortBy, (\\))
 import Data.Maybe (catMaybes, isNothing)
 import Data.Vector (Vector, fromList, (!))
 
@@ -10,18 +11,41 @@ data Input = Input
   }
 
 -- Returns the height at X/Y in the input
-height :: Input -> Int -> Int -> Int
-height input x y = numbers input ! (x + dx input * y)
+height :: Input -> (Int, Int) -> Int
+height input (x, y) = numbers input ! (x + dx input * y)
+
+-- Returns the neigboring points, preventing out-of-bounds access
+neigbors :: Input -> (Int, Int) -> [(Int, Int)]
+neigbors input (x, y) = catMaybes [left, right, above, below]
+  where
+    left = if x > 0 then Just (x - 1, y) else Nothing
+    right = if x + 1 < dx input then Just (x + 1, y) else Nothing
+    above = if y > 0 then Just (x, y - 1) else Nothing
+    below = if y + 1 < dy input then Just (x, y + 1) else Nothing
 
 -- Returns true if the point at X/Y is a low point among its neighbors
-lowpoint :: Input -> Int -> Int -> Bool
-lowpoint input x y = isNothing . find (<= lheight) $ catMaybes [left, right, above, below]
+lowpoint :: Input -> (Int, Int) -> Bool
+lowpoint input p = isNothing . find (<= lheight) $ nheights
   where
-    lheight = height input x y
-    left = if x > 0 then Just $ height input (x - 1) y else Nothing
-    right = if x + 1 < dx input then Just $ height input (x + 1) y else Nothing
-    above = if y > 0 then Just $ height input x (y - 1) else Nothing
-    below = if y + 1 < dy input then Just $ height input x (y + 1) else Nothing
+    lheight = height input p
+    nheights = map (height input) (neigbors input p)
+
+-- Returns the coordinates of all lowpoints in input
+lowpoints :: Input -> [(Int, Int)] -> Int -> [(Int, Int)]
+lowpoints input acc i
+  | i < length (numbers input) = lowpoints input next (i + 1)
+  | otherwise = acc
+  where
+    p = (i `mod` dx input, i `div` dx input)
+    next = if lowpoint input p then p : acc else acc
+
+-- Does a flood fill to determine all members in a "basin"
+basinSize :: Input -> [(Int, Int)] -> [(Int, Int)] -> Int
+basinSize _ from [] = (length . nub) from
+basinSize input from to = basinSize input (from ++ to) nps
+  where
+    cond x = height input x < 9
+    nps = nub $ filter cond $ concatMap (neigbors input) to \\ from
 
 parse :: [Char] -> Input
 parse content = Input dx dy indata
@@ -31,19 +55,25 @@ parse content = Input dx dy indata
     dy = length lns
     indata = fromList . map (read . pure :: Char -> Int) $ filter (not . isSpace) content
 
-partOne :: Input -> Int -> Int -> Int
-partOne input acc i
-  | i < length (numbers input) = partOne input (acc + risk) (i + 1)
-  | otherwise = acc
+partOne :: Input -> [(Int, Int)] -> Int
+partOne input = sum . map risk
   where
-    x = i `mod` dx input
-    y = i `div` dx input
-    h = height input x y
-    risk = if lowpoint input x y then 1 + h else 0
+    risk p = 1 + height input p
+
+partTwo :: Input -> [(Int, Int)] -> Int
+partTwo input points = product $ take 3 soredSizes
+  where
+    size x = basinSize input [] [x]
+    soredSizes = sortBy (flip compare) $ map size points
 
 main = do
   indata <- parse <$> readFile "input.txt"
+  let lpts = lowpoints indata [] 0
 
   putStrLn $
     "The answer to the first part is: "
-      ++ show (partOne indata 0 0)
+      ++ show (partOne indata lpts)
+
+  putStrLn $
+    "The answer to the second part is: "
+      ++ show (partTwo indata lpts)
