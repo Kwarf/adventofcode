5bb6a97794ed1ecd5f85e1e102ae32d4b2d9c935
Jimmy Bergström <me@kwarf.com>
2021-12-14 21:32:08 +0100

    Day 14, Part 2 - Haskell

--- 2021/day14/README.md
+++ 2021/day14/README.md
@@ -2,7 +2,12 @@
 
 This one seemed easier than many of the previous days.
 
+Okay never mind, it's obviously one of those days with a seemingly
+simple problem that you create a naive solution for that scales
+horribly. So I had to throw everything out and rewrite it for part 2.
+
 ```
 % cabal run -v0
 The answer to the first part is: 3906
+The answer to the second part is: 4441317262452
 ```
--- 2021/day14/main.hs
+++ 2021/day14/main.hs
@@ -1,44 +1,56 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# LANGUAGE TupleSections #-}
 
-import Data.Function (on)
-import Data.List (group, sort, sortBy)
+import Control.Arrow (first)
+import Data.List (sort)
 import Data.List.Split (splitOn)
+import Data.Map (Map, alter, empty, foldrWithKey, fromList, fromListWith, toList, unionWith)
 import Data.Maybe (fromJust)
+import Data.Tuple (swap)
 
-parseRules :: [String] -> [(String, String)]
-parseRules input = allRules
+parseRules :: [String] -> [(String, Char)]
+parseRules = map pair
   where
     split = splitOn " -> "
-    pair s = (head $ split s, head . last $ split s)
-    buildRule ([a, b], c) = ([a, b], [a, c])
-    allRules = map (buildRule . pair) input
+    pair s = ((head . split) s, (head . last . split) s)
 
+-- Returns substrings by pairs, i.e. "NNCB" -> ["NN", "NC", "CB", "B"]
 byTwo :: String -> [String]
 byTwo (a : b : xs) = [a, b] : byTwo (b : xs)
 byTwo [] = []
 byTwo x = [x]
 
-process :: [(String, String)] -> String -> String
-process rules = concatMap processPair . byTwo
+-- Used to create the initial map from the template
+seed :: String -> Map String Int
+seed string = foldr (alter increment) empty (byTwo string)
   where
-    processPair (a : b : _) = fromJust $ lookup [a, b] rules
-    processPair x = x
+    increment (Just x) = Just (x + 1)
+    increment Nothing = Just 1
 
-processSteps :: Int -> [(String, String)] -> String -> String
-processSteps 0 _ input = input
-processSteps n rules input = processSteps (n - 1) rules (process rules input)
-
-partOne :: [(String, String)] -> String -> Int
-partOne rules input = (length . last) groups - (length . head) groups
+-- Takes a pair and return the two resulting pairs
+resolve :: [(String, Char)] -> String -> [String]
+resolve _ [letter] = [[letter]]
+resolve rules pair = [[head pair, letter], [letter, last pair]]
   where
-    groups = sortBy (compare `on` length) $ group . sort $ processSteps 10 rules input
+    letter = fromJust $ lookup pair rules
+
+steps :: Int -> [(String, Char)] -> Map String Int -> Int
+steps n rules m = result $ foldr (const step) m [1..n]
+  where
+    step = foldrWithKey fn empty
+    fn p n m = unionWith (+) m ((fromList . map (,n)) $ resolve rules p)
+    result x = last (occurances x) - head (occurances x)
+    occurances = sort . map snd . toList . fromListWith (+) . map (first head) . toList
 
 main = do
   content <- lines <$> readFile "input.txt"
 
-  let template = head content
+  let template = seed . head $ content
   let rules = (parseRules . tail . tail) content
 
   putStrLn $
     "The answer to the first part is: "
-      ++ show (partOne rules template)
+      ++ show (steps 10 rules template)
+
+  putStrLn $
+    "The answer to the second part is: "
+      ++ show (steps 40 rules template)
