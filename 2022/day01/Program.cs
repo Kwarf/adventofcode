e8c1250324ef58a0578c7a73557c1c6b68336dfa
Jimmy Bergström <me@kwarf.com>
2022-12-01 18:10:51 +0100

    Day 1, Part 2 - C#

--- 2022/day01/Program.cs
+++ 2022/day01/Program.cs
@@ -1,7 +1,8 @@
 ﻿var elves = File.ReadAllText("input.txt")
     .Split($"{Environment.NewLine}{Environment.NewLine}")
-    .Select(x => new Elf(x.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()));
+    .Select(x => x.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Sum())
+    .OrderDescending()
+    .ToArray();
 
-Console.WriteLine($"The answer to the first part is: {elves.Select(x => x.Food.Sum()).Max()}");
-
-record struct Elf(List<int> Food);
+Console.WriteLine($"The answer to the first part is: {elves[0]}");
+Console.WriteLine($"The answer to the second part is: {elves.Take(3).Sum()}");
--- 2022/day01/README.md
+++ 2022/day01/README.md
@@ -5,4 +5,5 @@ Running with C# because I don't have a lot of time to set up something new today
 ```
 % dotnet run                                                                                                                                        √
 The answer to the first part is: 72070
+The answer to the second part is: 211805
 ```
