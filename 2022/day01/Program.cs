f09748abf3fb84227a9532566840b377f3e19d14
Jimmy Bergström <me@kwarf.com>
2022-12-01 18:06:56 +0100

    Day 1, Part 1 - C#

---
+++ 2022/day01/Program.cs
@@ -0,0 +1,7 @@
+﻿var elves = File.ReadAllText("input.txt")
+    .Split($"{Environment.NewLine}{Environment.NewLine}")
+    .Select(x => new Elf(x.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()));
+
+Console.WriteLine($"The answer to the first part is: {elves.Select(x => x.Food.Sum()).Max()}");
+
+record struct Elf(List<int> Food);
