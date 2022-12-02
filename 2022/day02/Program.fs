9ffca4c63c34e9179e7bd541b497b49f1ad83ecd
Jimmy Bergström <me@kwarf.com>
2022-12-02 10:06:39 +0100

    Day 2, Part 1 - F#

---
+++ 2022/day02/Program.fs
@@ -0,0 +1,13 @@
+﻿open System.IO
+
+let score (a, b) =
+    let b = char (int b - 23)
+    match a, b with
+    | a, b when a = b -> 3
+    | 'A', 'B' | 'B', 'C' | 'C', 'A' -> 6
+    | _ -> 0
+    + int b - 64
+
+printfn
+    "The answer to the first part is: %i"
+    (File.ReadAllLines("input.txt") |> Array.sumBy (score << fun s -> (s.Chars 0, s.Chars 2)))
---
+++ 2022/day02/README.md
@@ -0,0 +1,8 @@
+# --- Day 2: Rock Paper Scissors ---
+
+F# today, more fun than C#, less pain to set up than Haskell.
+
+```
+% dotnet run
+The answer to the first part is: 8933
+```
