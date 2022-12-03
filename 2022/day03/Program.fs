d05b5016d9bcfbd50365034af6153e85e4177b8b
Jimmy Bergström <me@kwarf.com>
2022-12-03 18:06:17 +0100

    Day 3, Part 1 - F#

---
+++ 2022/day03/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+open System.IO
+
+let duplicates sack =
+    let len = String.length sack / 2
+    Set.intersect (Set.ofSeq sack.[..len-1]) (Set.ofSeq sack.[len..])
+    |> Set.toArray
+
+let priority c = if Char.IsLower c then int c - 96 else int c - 38
+
+printfn
+    "The answer to the first part is: %i"
+    (File.ReadAllLines("input.txt") |> Array.sumBy (Array.sumBy priority << duplicates))
