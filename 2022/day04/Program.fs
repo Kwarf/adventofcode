1dd62105a18a45ea88aa6ad20e59549147dbae78
Jimmy Bergström <me@kwarf.com>
2022-12-04 11:50:40 +0100

    Day 4, Part 1 - F#

---
+++ 2022/day04/Program.fs
@@ -0,0 +1,19 @@
+﻿open System.IO
+
+let range (s: string) =
+    let parts = s.Split '-' |> Array.map int
+    (parts[0], parts[1])
+
+let parseRow (s: string) =
+    let elves = s.Split ',' |> Array.map range
+    (elves[0], elves[1])
+
+let isOverlapping (r1, r2) =
+    (fst r1 >= fst r2 && snd r1 <= snd r2) ||
+    (fst r2 >= fst r1 && snd r2 <= snd r1)
+
+printfn
+    "The answer to the first part is: %i"
+    (File.ReadAllLines("input.txt")
+     |> Array.where (isOverlapping << parseRow)
+     |> Array.length)
---
+++ 2022/day04/README.md
@@ -0,0 +1,8 @@
+# --- Day 4: Camp Cleanup ---
+
+Seemed easy enough to just keep going with F#.
+
+```
+% dotnet run
+The answer to the first part is: 584
+```
