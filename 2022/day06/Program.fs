867eb96af722cd2e1366d53f48bb9021e4130a47
Jimmy Bergström <me@kwarf.com>
2022-12-06 11:05:16 +0000

    Day 6, Part 1 - F#

---
+++ 2022/day06/Program.fs
@@ -0,0 +1,9 @@
+﻿open System.IO
+
+let rec findMarker i (s: string) =
+    if s[i - 4 .. i - 1] |> Seq.distinct |> Seq.length = 4 then i
+    else findMarker (i + 1) s
+
+printfn
+    "The answer to the first part is: %i"
+    (findMarker 4 (File.ReadAllLines("input.txt")[0]))
