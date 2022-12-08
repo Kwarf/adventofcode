4c7e2ed294a4a1b1ad87b384423569ad2a21a317
Jimmy Bergström <me@kwarf.com>
2022-12-08 20:37:42 +0100

    Day 8, Part 2 - F#

--- 2022/day08/Program.fs
+++ 2022/day08/Program.fs
@@ -3,31 +3,46 @@
 let input =
     let number c = int c - int '0'
     File.ReadAllLines("input.txt")
-    |> Array.map (fun x -> x |> Seq.map number |> Seq.toArray)
+    |> Array.map (Seq.map number << Seq.toArray)
     |> array2D
 
-let visible (grid: int[,]) (x, y) =
-    let get = Array2D.get grid
-    let isLower = (>) (get y x)
-    match x, y with
-    | _, 0 | 0, _ -> true
-    | x, y when x + 1 = Array2D.length2 grid || y + 1 = Array2D.length1 grid -> true
-    | _ -> seq { x + 1 .. Array2D.length2 grid - 1 }
-            |> Seq.map (fun x -> get y x)
-            |> Seq.forall isLower
-        || seq { x - 1 .. -1 .. 0 }
-            |> Seq.map (fun x -> get y x)
-            |> Seq.forall isLower
-        || seq { y + 1 .. Array2D.length1 grid - 1 }
-            |> Seq.map (fun y -> get y x)
-            |> Seq.forall isLower
-        || seq { y - 1 .. -1 .. 0 }
-            |> Seq.map (fun y -> get y x)
-            |> Seq.forall isLower
+let width = (+) -1 << Array2D.length2
+let height = (+) -1 << Array2D.length1
 
-let indices (grid: int[,]) = seq { for x in 0 .. Array2D.length2 grid - 1 do
-                                   for y in 0 .. Array2D.length1 grid - 1 do yield x, y }
+let visible grid (x, y) =
+    let at = Array2D.get grid
+    let isLower = (>) (at y x)
+    match x, y with
+    | _, 0
+    | 0, _ -> true
+    | x, y when x = width grid || y = height grid -> true
+    | _ ->
+        [ { x + 1 .. width grid } |> Seq.map (fun x -> at y x)
+          { y + 1 .. height grid } |> Seq.map (fun y -> at y x)
+          { x - 1 .. -1 .. 0 } |> Seq.map (fun x -> at y x)
+          { y - 1 .. -1 .. 0 } |> Seq.map (fun y -> at y x) ]
+        |> Seq.fold (fun acc x -> acc || x |> Seq.forall isLower) false
+
+let score grid (x, y) =
+    let at = Array2D.get grid
+    let rec range n ts =
+        match ts with
+        | ts when Seq.isEmpty ts -> n
+        | ts when Seq.head ts >= (at x y) -> n + 1
+        | _ -> range (n + 1) (Seq.skip 1 ts)
+    [ { x + 1 .. width grid } |> Seq.map (fun x -> at y x)
+      { y + 1 .. height grid } |> Seq.map (fun y -> at y x)
+      { x - 1 .. -1 .. 0 } |> Seq.map (fun x -> at y x)
+      { y - 1 .. -1 .. 0 } |> Seq.map (fun y -> at y x) ]
+    |> Seq.fold (fun acc x -> acc * range 0 x) 1
+
+let indices grid = seq { for x in 0 .. width grid do
+                         for y in 0 .. height grid do yield x, y }
 
 printfn
     "The answer to the first part is: %i"
     (input |> indices |> Seq.where (visible input) |> Seq.length)
+
+printfn
+    "The answer to the second part is: %i"
+    (input |> indices |> Seq.map (score input) |> Seq.max)
--- 2022/day08/README.md
+++ 2022/day08/README.md
@@ -5,4 +5,5 @@ Back on F#.
 ```
 % dotnet run
 The answer to the first part is: 1807
+The answer to the second part is: 480000
 ```
