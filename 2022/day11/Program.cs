32b6c7e0ed77175265437bee9cd7b39aa114aa70
Jimmy Bergström <me@kwarf.com>
2022-12-11 15:10:00 +0100

    Day 11, Part 2 - C#

--- 2022/day11/Program.cs
+++ 2022/day11/Program.cs
@@ -1,44 +1,55 @@
-﻿Func<int, int, int> Op(char op) => op switch
+﻿Func<long, long, long> Op(char op) => op switch
 {
-    '+' => (int a, int b) => a + b,
-    '*' => (int a, int b) => a * b,
+    '+' => (long a, long b) => a + b,
+    '*' => (long a, long b) => a * b,
 };
 
-var monkeys = File.ReadAllLines("input.txt")
+List<Monkey> Monkeys() => File.ReadAllLines("input.txt")
     .Chunk(7)
     .Select(line =>
     {
-        Func<int, int> op = line[2].Substring(19).Split(' ') switch
+        Func<long, long> op = line[2].Substring(19).Split(' ') switch
         {
-            ["old", var x, "old"] => (int old) => Op(x[0])(old, old),
-            ["old", var x, var n] => (int old) => Op(x[0])(old, Int32.Parse(n)),
+            ["old", var x, "old"] => (long old) => Op(x[0])(old, old),
+            ["old", var x, var n] => (long old) => Op(x[0])(old, Int32.Parse(n)),
         };
 
-        return new Monkey(new Queue<int>(line[1].Substring(18).Split(", ").Select<string, int>(int.Parse))
-            , (monkeys, item) =>
+        return new Monkey(new Queue<long>(line[1].Substring(18).Split(", ").Select<string, long>(long.Parse))
+            , (monkeys, item, worryOp) =>
             {
-                var worry = op(item) / 3;
+                var worry = worryOp(op(item));
                 var target = worry % Int32.Parse(line[3].Substring(21)) == 0
                     ? Int32.Parse(line[4].Substring(29))
                     : Int32.Parse(line[5].Substring(30));
                 monkeys[target].Items.Enqueue(worry);
             }
-            , 0);
+            , 0
+            , Int32.Parse(line[3].Substring(21)));
     })
     .ToList();
 
-for (var round = 0; round < 20; round++)
+long Part(long part)
 {
-    for (var i = 0; i < monkeys.Count; i++)
+    var monkeys = Monkeys();
+    Func<long, long> worry = part == 1
+        ? (long x) => x / 3
+        : (long x) => x % monkeys.Aggregate(1L, (acc, x) => acc * x.Check);
+
+    for (var round = 0; round < (part == 1 ? 20 : 10000); round++)
     {
-        while (monkeys[i].Items.TryDequeue(out var item))
+        for (var i = 0; i < monkeys.Count; i++)
         {
-            monkeys[i].Action(monkeys, item);
-            monkeys[i] = monkeys[i] with { Inspected = monkeys[i].Inspected + 1 };
+            while (monkeys[i].Items.TryDequeue(out var item))
+            {
+                monkeys[i].Action(monkeys, item, worry);
+                monkeys[i] = monkeys[i] with { Inspected = monkeys[i].Inspected + 1 };
+            }
         }
     }
+    return monkeys.Select(x => x.Inspected).OrderDescending().Take(2).Aggregate(1L, (acc, x) => acc * x);
 }
 
-Console.WriteLine($"The answer to the first part is: {monkeys.Select(x => x.Inspected).OrderDescending().Take(2).Aggregate(1, (acc, x) => acc * x)}");
+Console.WriteLine($"The answer to the first part is: {Part(1)}");
+Console.WriteLine($"The answer to the second part is: {Part(2)}");
 
-record struct Monkey(Queue<int> Items, Action<List<Monkey>, int> Action, int Inspected);
+record struct Monkey(Queue<long> Items, Action<List<Monkey>, long, Func<long, long>> Action, long Inspected, long Check);
--- 2022/day11/README.md
+++ 2022/day11/README.md
@@ -5,4 +5,5 @@ C# because it felt like I would have to hold a bunch of state.
 ```
 % dotnet run
 The answer to the first part is: 66124
+The answer to the second part is: 19309892877
 ```
