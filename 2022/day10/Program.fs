fac5429c3f07fdccc58c53785abc1e2d21d1725c
Jimmy Bergström <me@kwarf.com>
2022-12-11 03:18:25 +0100

    Day 10, Part 1 - F#

---
+++ 2022/day10/Program.fs
@@ -0,0 +1,28 @@
+﻿let (|Op|_|) (c: string) (s: string) = if s.StartsWith(c) then Some(s.Substring(c.Length)) else None
+
+type Instruction =
+    | Noop
+    | Addx of int * int
+
+type CPU = int * Instruction list
+
+let parse line =
+    match line with
+    | Op "addx" n -> Addx (1, int n)
+    | _ -> Noop
+
+let program = System.IO.File.ReadAllLines("input.txt") |> Array.map parse |> List.ofArray
+
+let cycle cpu =
+    match List.head (snd cpu) with
+    | Addx (0, num) -> CPU(fst cpu + num, List.tail (snd cpu))
+    | Addx (clk, num) -> CPU(fst cpu, Addx(clk - 1, num) :: List.tail (snd cpu))
+    | Noop -> CPU(fst cpu, List.tail (snd cpu))
+
+printfn
+    "The answer to the first part is: %i"
+    ({ 1..220 }
+    |> Seq.fold
+        (fun (sum, x) n -> ((if n = 20 || n >= 60 && (n - 20) % 40 = 0 then (sum + (n * fst x)) else sum), cycle x))
+        (0, (CPU(1, program)))
+    |> fst)
