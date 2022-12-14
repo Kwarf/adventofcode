8735cbf7af481b5abb53d1922e7d74ba37670552
Jimmy Bergström <me@kwarf.com>
2022-12-14 22:06:29 +0100

    Day 14, Part 1 - C#

---
+++ 2022/day14/Program.cs
@@ -0,0 +1,47 @@
+﻿IEnumerable<int> Range(int from, int to) => Enumerable.Range(0, Math.Abs(to - from) + 1).Select(x => from + (x * Math.Sign(to - from)));
+
+IEnumerable<(int, int)> Interpolate((int, int) from, (int, int) to) => (from, to) switch
+{
+    ((var x1, var y1), (var x2, var y2)) when x1 != x2 && y1 == y2 => Range(x1, x2).Select(x => (x, y1)),
+    ((var x1, var y1), (var x2, var y2)) when x1 == x2 && y1 != y2 => Range(y1, y2).Select(y => (x1, y)),
+    _ => new[] {from, to},
+};
+
+IEnumerable<(int X, int Y)> ParseLine(string line) => line
+    .Split(new[] { ",", " -> " }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(Int32.Parse)
+    .Chunk(2)
+    .Select(x => (x[0], x[1]))
+    .Aggregate(new List<(int, int)>(), ((acc, x) => acc.Count switch
+    {
+        0 => new List<(int, int)> { x },
+        _ => acc.Concat(Interpolate(acc[acc.Count - 1], x)).ToList(),
+    }));
+
+ISet<(int, int)> Simulate(int floor, ISet<(int, int)> rocks)
+{
+    var sand = new HashSet<(int, int)>();
+    while (true)
+    {
+        var s = (X: 500, Y: 0);
+        while (true)
+        {
+            if (s.Y > floor) return sand;
+            s = s with { Y = s.Y + 1};
+            if (!rocks.Contains(s) && !sand.Contains(s)) continue;
+            s = s with { X = s.X - 1};
+            if (!rocks.Contains(s) && !sand.Contains(s)) continue;
+            s = s with { X = s.X + 2};
+            if (!rocks.Contains(s) && !sand.Contains(s)) continue;
+            s = s with { X = s.X - 1, Y = s.Y - 1};
+            sand.Add(s);
+            break;
+        }
+    }
+}
+
+var input = File.ReadAllLines("input.txt")
+    .SelectMany(ParseLine)
+    .ToHashSet();
+
+Console.WriteLine($"The answer to the first part is: {Simulate(input.Select(x => x.Y).Max(), input).Count()}");
---
+++ 2022/day14/README.md
@@ -0,0 +1,8 @@
+# --- Day 14: Regolith Reservoir ---
+
+C# because meh.
+
+```
+% dotnet run
+The answer to the first part is: 683
+```
