9901ef79696a9cc48afd33aa6a830c1e3da330e1
Jimmy Bergström <me@kwarf.com>
2023-12-02 13:49:38 +0100

    Day 2, Part 1 - Rust

---
+++ 2023/day02/README.md
@@ -0,0 +1,6 @@
+# --- Day 2: Cube Conundrum ---
+
+```
+% cargo run --release -q
+The answer to the first part is: 2377
+```
---
+++ 2023/day02/src/main.rs
@@ -0,0 +1,62 @@
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+    ops::Index,
+};
+
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref REGEX: Regex = Regex::new(r"(\d+)\s(\w+)").unwrap();
+}
+
+#[derive(PartialEq, Eq, Hash)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+type Game = Vec<(u32, Color)>;
+
+fn parse_game(line: &str) -> Game {
+    REGEX
+        .captures_iter(line)
+        .map(|x| x.extract())
+        .map(|(_, [count, color])| match color {
+            "red" => (count.parse().unwrap(), Color::Red),
+            "green" => (count.parse().unwrap(), Color::Green),
+            "blue" => (count.parse().unwrap(), Color::Blue),
+            _ => panic!(),
+        })
+        .collect()
+}
+
+fn is_possible<'a>(bag: &impl Index<&'a Color, Output = u32>, game: &'a Game) -> bool {
+    game.iter().all(|x| bag[&x.1] >= x.0)
+}
+
+fn main() {
+    let bag = HashMap::from([(Color::Red, 12), (Color::Green, 13), (Color::Blue, 14)]);
+
+    let file = File::open("input.txt").unwrap();
+    let games = BufReader::new(file)
+        .lines()
+        .map(|x| parse_game(&x.unwrap()))
+        .collect::<Vec<Game>>();
+
+    println!(
+        "The answer to the first part is: {}",
+        games
+            .iter()
+            .enumerate()
+            .filter_map(|(i, game)| if is_possible(&bag, game) {
+                Some(i + 1)
+            } else {
+                None
+            })
+            .sum::<usize>()
+    );
+}
