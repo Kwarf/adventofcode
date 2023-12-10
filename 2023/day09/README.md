b5529dff27c55c4e07d4b54cf82f6c23b0bc86b8
Jimmy Bergström <me@kwarf.com>
2023-12-10 04:21:04 +0100

    Day 9, Part 2 - Rust

--- 2023/day09/README.md
+++ 2023/day09/README.md
@@ -1,6 +1,9 @@
 # --- Day 9: Mirage Maintenance ---
 
+This one was a bit too easy now wasn't it?
+
 ```
 % cargo run --release -q
 The answer to the first part is: 1995001648
+The answer to the second part is: 988
 ```
--- 2023/day09/src/main.rs
+++ 2023/day09/src/main.rs
@@ -5,25 +5,51 @@ use std::{
 
 type Sequence = Vec<isize>;
 
-fn next(sequence: &Sequence) -> isize {
+#[derive(Clone, Copy)]
+enum Direction {
+    Forward,
+    Backward,
+}
+
+fn next(sequence: &Sequence, direction: Direction) -> isize {
     if sequence.iter().all(|x| *x == 0) {
         return 0;
     }
 
-    sequence.last().unwrap() + next(&sequence.windows(2).map(|xs| xs[1] - xs[0]).collect())
+    let next = next(
+        &sequence.windows(2).map(|xs| xs[1] - xs[0]).collect(),
+        direction,
+    );
+
+    match direction {
+        Direction::Forward => sequence.last().unwrap() + next,
+        Direction::Backward => sequence.first().unwrap() - next,
+    }
 }
 
 fn main() {
     let file = File::open("input.txt").unwrap();
-    let sequences = BufReader::new(file).lines().map(|line| {
-        line.unwrap()
-            .split_ascii_whitespace()
-            .map(|x| x.parse::<isize>().unwrap())
-            .collect::<Sequence>()
-    });
+    let sequences: Vec<Sequence> = BufReader::new(file)
+        .lines()
+        .map(|line| {
+            line.unwrap()
+                .split_ascii_whitespace()
+                .map(|x| x.parse::<isize>().unwrap())
+                .collect::<Sequence>()
+        })
+        .collect();
 
     println!(
         "The answer to the first part is: {}",
-        sequences.map(|x| next(&x)).sum::<isize>()
+        sequences
+            .iter()
+            .fold(0, |acc, x| acc + next(x, Direction::Forward))
+    );
+
+    println!(
+        "The answer to the second part is: {}",
+        sequences
+            .iter()
+            .fold(0, |acc, x| acc + next(x, Direction::Backward))
     );
 }
