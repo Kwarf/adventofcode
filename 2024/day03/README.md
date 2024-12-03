aa6f977c275f68f182c8d43e7cbdb40c15e9314c
Jimmy Bergström <me@kwarf.com>
2024-12-03 22:17:14 +0100

    Day 3, Part 2 - Rust

--- 2024/day03/README.md
+++ 2024/day03/README.md
@@ -5,4 +5,5 @@ All right, fine, I'll use regex.
 ```
 % cargo run --release -q
 The answer to the first part is: 184511516
+The answer to the second part is: 90044227
 ```
--- 2024/day03/src/main.rs
+++ 2024/day03/src/main.rs
@@ -1,18 +1,44 @@
 use std::fs::read_to_string;
 
+use itertools::Itertools;
 use regex::Regex;
 
+fn process(s: &str) -> u64 {
+    Regex::new(r"mul\((\d+),(\d+)\)")
+        .unwrap()
+        .captures_iter(s)
+        .map(|c| {
+            let (_, [a, b]) = c.extract();
+            a.parse::<u64>().unwrap() * b.parse::<u64>().unwrap()
+        })
+        .sum()
+}
+
 fn main() {
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     let input = read_to_string("input.txt").unwrap();
 
+    println!("The answer to the first part is: {}", process(&input));
+
+    let controls = input
+        .match_indices("don't()")
+        .map(|(i, _)| (i, false))
+        .chain(input.match_indices("do()").map(|(i, _)| (i, true)))
+        .sorted_by_key(|(i, _)| *i)
+        .collect::<Vec<(usize, bool)>>();
+
     println!(
-        "The answer to the first part is: {}",
-        re.captures_iter(&input)
-            .map(|c| {
-                let (_, [a, b]) = c.extract();
-                a.parse().unwrap_or(0) * b.parse().unwrap_or(0)
-            })
-            .sum::<i64>()
+        "The answer to the second part is: {}",
+        controls
+            .iter()
+            .tuple_windows()
+            .filter(|(a, _)| a.1)
+            .map(|(a, b)| process(&input[a.0..b.0]))
+            .sum::<u64>()
+            + process(&input[..controls[0].0])
+            + if controls.last().unwrap().1 {
+                process(&input[controls.last().unwrap().0..])
+            } else {
+                0
+            }
     );
 }
