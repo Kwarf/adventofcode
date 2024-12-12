95672230a678b932d1974bfc350716d9fa50b7f0
Jimmy Bergström <me@kwarf.com>
2024-12-12 22:00:47 +0100

    Day 8, Part 2 - Rust

--- 2024/day08/README.md
+++ 2024/day08/README.md
@@ -5,4 +5,5 @@ No optimization, no brainpower, only recursive searches.
 ```
 % cargo run --release -q
 The answer to the first part is: 289
+The answer to the second part is: 1030
 ```
--- 2024/day08/src/main.rs
+++ 2024/day08/src/main.rs
@@ -1,63 +1,41 @@
-use std::{
-    collections::HashMap,
-    fmt::{Display, Formatter},
-    fs::read_to_string,
-};
+use std::{collections::HashMap, fs::read_to_string};
+
+use itertools::Itertools;
 
 struct Map {
     width: usize,
     height: usize,
-    antennas: HashMap<(u8, u8), char>,
-}
-
-impl Display for Map {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let pos = (x as u8, y as u8);
-                if let Some(antenna) = self.antennas.get(&pos) {
-                    write!(f, "{}", antenna)?;
-                } else {
-                    write!(f, "{}", if self.is_antinode(pos) { '#' } else { '.' })?;
-                }
-            }
-            writeln!(f)?;
-        }
-        Ok(())
-    }
+    antennas: HashMap<char, Vec<(i8, i8)>>,
 }
 
 impl Map {
-    fn positions<'a>(&'a self) -> impl Iterator<Item = (u8, u8)> + 'a {
-        (0..self.width as u8).flat_map(|x| (0..self.height as u8).map(move |y| (x, y)))
+    fn positions<'a>(&'a self) -> impl Iterator<Item = (i8, i8)> + 'a {
+        (0..self.width as i8).flat_map(|x| (0..self.height as i8).map(move |y| (x, y)))
     }
 
-    fn is_antinode(&self, pos: (u8, u8)) -> bool {
-        self.antennas
-            .iter()
-            .fold(HashMap::new(), |mut map, (p, antenna)| {
-                map.entry(*antenna).or_insert_with(Vec::new).push(*p);
-                map
+    fn is_out_of_bounds(&self, pos: (i8, i8)) -> bool {
+        pos.0 < 0 || pos.0 >= self.width as i8 || pos.1 < 0 || pos.1 >= self.height as i8
+    }
+
+    fn is_antinode(&self, pos: (i8, i8)) -> bool {
+        self.antennas.iter().any(|(_, antennas)| {
+            antennas.iter().any(|antenna_pos| {
+                let d = distance(*antenna_pos, pos);
+                d > 0
+                    && antennas.iter().any(|other_antenna_pos| {
+                        d == distance(*other_antenna_pos, pos) * 2
+                            && is_collinear(&[pos, *antenna_pos, *other_antenna_pos])
+                    })
             })
-            .iter()
-            .any(|(_, antennas)| {
-                antennas.iter().any(|antenna_pos| {
-                    let d = distance(*antenna_pos, pos);
-                    d > 0
-                        && antennas.iter().any(|other_antenna_pos| {
-                            d == distance(*other_antenna_pos, pos) * 2
-                                && is_collinear(&[pos, *antenna_pos, *other_antenna_pos])
-                        })
-                })
-            })
+        })
     }
 }
 
-fn distance(a: (u8, u8), b: (u8, u8)) -> u8 {
-    (a.0 as i8 - b.0 as i8).abs() as u8 + (a.1 as i8 - b.1 as i8).abs() as u8
+fn distance(a: (i8, i8), b: (i8, i8)) -> i8 {
+    (a.0 - b.0).abs() + (a.1 - b.1).abs()
 }
 
-fn is_collinear(positions: &[(u8, u8)]) -> bool {
+fn is_collinear(positions: &[(i8, i8)]) -> bool {
     if positions.len() <= 2 {
         return true;
     }
@@ -83,7 +61,10 @@ fn main() {
             map.width = line.len();
             map.height += 1;
             for (x, c) in line.chars().enumerate().filter(|(_, c)| *c != '.') {
-                map.antennas.insert((x as u8, y as u8), c);
+                map.antennas
+                    .entry(c)
+                    .or_insert_with(Vec::new)
+                    .push((x as i8, y as i8));
             }
             map
         },
@@ -92,5 +73,22 @@ fn main() {
     println!(
         "The answer to the first part is: {}",
         map.positions().filter(|&pos| map.is_antinode(pos)).count()
+    );
+
+    println!(
+        "The answer to the second part is: {}",
+        map.antennas
+            .iter()
+            .flat_map(|(_, antennas)| antennas.iter().permutations(2))
+            .flat_map(|combination| {
+                let (a, b) = (combination[0], combination[1]);
+                let (dx, dy) = (b.0 - a.0, b.1 - a.1);
+                (a.0..)
+                    .step_by(dx as usize)
+                    .zip((a.1..).step_by(dy as usize))
+                    .take_while(|&pos| !map.is_out_of_bounds(pos))
+            })
+            .unique()
+            .count()
     );
 }
