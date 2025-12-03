50b436de0e2b4e89ba9326547e064a4916df269d
Jimmy Bergström <me@kwarf.com>
2025-12-03 09:39:28 +0100

    Day 3, Part 2 - Rust

--- 2025/day03/README.md
+++ 2025/day03/README.md
@@ -5,4 +5,5 @@ Letter by letter, turning batteries better.
 ```
 % cargo run --release -q
 The answer to the first part is: 17435
+The answer to the second part is: 172886048065379
 ```
--- 2025/day03/src/main.rs
+++ 2025/day03/src/main.rs
@@ -1,15 +1,18 @@
-fn joltage(battery: &[char]) -> u32 {
-    battery
-        .iter()
-        .enumerate()
-        .flat_map(|(i, &first)| {
-            battery
-                .iter()
-                .skip(i + 1)
-                .map(move |&second| first.to_digit(10).unwrap() * 10 + second.to_digit(10).unwrap())
-        })
-        .max()
-        .unwrap()
+fn joltage(cells: usize, battery: &[char]) -> u64 {
+    let mut stack = Vec::with_capacity(cells);
+    for (i, &digit) in battery.iter().enumerate() {
+        while let Some(&last) = stack.last() {
+            if digit <= last || stack.len() + battery.len() - i - 1 < cells {
+                break;
+            }
+            stack.pop();
+        }
+
+        if stack.len() < cells {
+            stack.push(digit);
+        }
+    }
+    stack.into_iter().collect::<String>().parse().unwrap()
 }
 
 fn main() {
@@ -20,7 +23,15 @@ fn main() {
         "The answer to the first part is: {}",
         batteries
             .iter()
-            .map(|b| joltage(&b.chars().collect::<Vec<_>>()))
-            .sum::<u32>()
+            .map(|b| joltage(2, &b.chars().collect::<Vec<_>>()))
+            .sum::<u64>()
+    );
+
+    println!(
+        "The answer to the second part is: {}",
+        batteries
+            .iter()
+            .map(|b| joltage(12, &b.chars().collect::<Vec<_>>()))
+            .sum::<u64>()
     );
 }
