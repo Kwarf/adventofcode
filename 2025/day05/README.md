87b90cae592d12a64b754ab2098487430a1143ba
Jimmy Bergström <me@kwarf.com>
2025-12-05 16:03:56 +0100

    Day 5, Part 1 - Rust

---
+++ 2025/day05/README.md
@@ -0,0 +1,9 @@
+# --- Day 5: Cafeteria ---
+
+I thought the puzzles would be way harder by now given that there's only going
+to be 12 days.
+
+```
+% cargo run --release -q
+The answer to the first part is: 840
+```
---
+++ 2025/day05/src/main.rs
@@ -0,0 +1,31 @@
+use std::ops::RangeInclusive;
+
+fn parse_input(input: &str) -> (Vec<RangeInclusive<usize>>, Vec<usize>) {
+    let mut input_parts = input.split("\n\n");
+    let ranges = input_parts.next().unwrap();
+    let available_ids = input_parts.next().unwrap();
+    (
+        ranges
+            .lines()
+            .map(|line| {
+                let (start, end) = line.split_once('-').unwrap();
+                start.parse().unwrap()..=end.parse().unwrap()
+            })
+            .collect(),
+        available_ids
+            .lines()
+            .map(|line| line.parse().unwrap())
+            .collect(),
+    )
+}
+
+fn main() {
+    let (database, ids) = parse_input(&std::fs::read_to_string("input.txt").unwrap());
+
+    println!(
+        "The answer to the first part is: {}",
+        ids.iter()
+            .filter(|id| database.iter().any(|range| range.contains(id)))
+            .count()
+    );
+}
