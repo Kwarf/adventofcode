e03d478003372ae1206480377afb4c2afc86f4c1
Jimmy Bergström <me@kwarf.com>
2025-12-09 22:14:52 +0100

    Day 9, Part 2 - Rust

--- 2025/day09/README.md
+++ 2025/day09/README.md
@@ -2,7 +2,10 @@
 
 Part 1 was suspiciously easy.
 
+Part 2 broke my brain until it all clicked.
+
 ```
 % cargo run --release -q
 The answer to the first part is: 4781546175
+The answer to the second part is: 1573359081
 ```
--- 2025/day09/src/main.rs
+++ 2025/day09/src/main.rs
@@ -26,13 +26,50 @@ fn main() {
         })
         .collect();
 
+    let candidates: Vec<_> = points
+        .iter()
+        .tuple_combinations()
+        .sorted_by_key(|(p1, p2)| -p1.area(p2))
+        .collect();
+
     println!(
         "The answer to the first part is: {}",
-        points
+        candidates[0].0.area(candidates[0].1)
+    );
+
+    println!(
+        "The answer to the second part is: {}",
+        candidates
             .iter()
-            .combinations(2)
-            .map(|pair| pair[0].area(pair[1]))
-            .max()
+            .find_map(|(p1, p2)| {
+                let (x_min, x_max, y_min, y_max) = (
+                    p1.x.min(p2.x),
+                    p1.x.max(p2.x),
+                    p1.y.min(p2.y),
+                    p1.y.max(p2.y),
+                );
+
+                let n = points.len();
+                for i in 0..n {
+                    let a = &points[i];
+                    let b = &points[(i + 1) % n];
+
+                    if (a.x == b.x
+                        && a.x > x_min
+                        && a.x < x_max
+                        && a.y.max(b.y) > y_min
+                        && a.y.min(b.y) < y_max)
+                        || (a.y == b.y
+                            && a.y > y_min
+                            && a.y < y_max
+                            && a.x.max(b.x) > x_min
+                            && a.x.min(b.x) < x_max)
+                    {
+                        return None;
+                    }
+                }
+                Some(p1.area(p2))
+            })
             .unwrap()
     );
 }
